aed690aef407fd2f0e07522a20a359bc76ee2f96
kaswiatkowski <kaswiatkowski@gmail.com>
2020-06-26 09:39:38 +0200

    Add DynamicNodeProvider (MvcSiteMapProvider)

---
+++ ImpressDev/Infrastructure/BooksDynamicNodeProvider.cs
@@ -0,0 +1,28 @@
+﻿using ImpressDev.DAL;
+using ImpressDev.Models;
+using MvcSiteMapProvider;
+using System.Collections.Generic;
+
+namespace ImpressDev.Infrastructure
+{
+    public class BooksDynamicNodeProvider : DynamicNodeProviderBase
+    {
+        private ImpressDevContext db = new ImpressDevContext();
+
+        public override IEnumerable<DynamicNode> GetDynamicNodeCollection(ISiteMapNode nodee)
+        {
+            var returnValue = new List<DynamicNode>();
+
+            foreach (Book book in db.Books)
+            {
+                DynamicNode node = new DynamicNode();
+                node.Title = book.Title;
+                node.Key = "Book_" + book.BookId;
+                node.ParentKey = "Category_" + book.CategoryId;
+                node.RouteValues.Add("bookId", book.BookId);
+                returnValue.Add(node);
+            }
+            return returnValue;
+        }
+    }
+}
---
+++ ImpressDev/Infrastructure/CategoriesDynamicNodeProvider.cs
@@ -0,0 +1,27 @@
+﻿using ImpressDev.DAL;
+using ImpressDev.Models;
+using MvcSiteMapProvider;
+using System.Collections.Generic;
+
+namespace ImpressDev.Infrastructure
+{
+    public class CategoriesDynamicNodeProvider : DynamicNodeProviderBase
+    {
+        private ImpressDevContext db = new ImpressDevContext();
+
+        public override IEnumerable<DynamicNode> GetDynamicNodeCollection(ISiteMapNode nodee)
+        {
+            var returnValue = new List<DynamicNode>();
+
+            foreach (Category category in db.Categories)
+            {
+                DynamicNode node = new DynamicNode();
+                node.Title = category.Name;
+                node.Key = "Category_" + category.CategoryId;
+                node.RouteValues.Add("categoryName", category.Name);
+                returnValue.Add(node);
+            }
+            return returnValue;
+        }
+    }
+}
