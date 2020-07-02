4f6c7912f66d6f9fd3000534cc50dea2d653fb2d
kaswiatkowski <kaswiatkowski@gmail.com>
2020-07-02 12:28:32 +0200

    Add Login View and modify style

---
+++ ImpressDev/ViewModels/AccountViewModels.cs
@@ -0,0 +1,37 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ImpressDev.ViewModels
+{
+    public class LoginViewModel
+    {
+        [Required(ErrorMessage = "Wprowadź adres email")]
+        [EmailAddress(ErrorMessage = "Błędny format adresu email")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Wprowadź hasło")]
+        [DataType(DataType.Password)]
+        [Display(Name = "Hasło")]
+        public string Password { get; set; }
+
+        [Display(Name = "Zapamiętaj dane logowania")]
+        public bool Remember { get; set; }
+    }
+
+    public class RegisterViewModel
+    {
+        [Required(ErrorMessage = "Wprowadź adres email")]
+        [EmailAddress(ErrorMessage = "Błędny format adresu email")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Wprowadź hasło")]
+        [StringLength(30, ErrorMessage = "Hasło musi mieć od 5 do 30 znaków", MinimumLength = 5)]
+        [DataType(DataType.Password)]
+        [Display(Name = "Hasło")]
+        public string Password { get; set; }
+
+        [Display(Name = "Potwierdź hasło")]
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "Hasło i potwierdzenie hasła muszą być takie same")]
+        public string ConfirmPassword { get; set; }
+    }
+}
