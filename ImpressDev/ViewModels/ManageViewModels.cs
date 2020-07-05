b08d5f09d2384c996fd55effcd1a154e2f134371
kaswiatkowski <kaswiatkowski@gmail.com>
2020-07-05 17:01:22 +0200

    Add manage profile and password change

---
+++ ImpressDev/ViewModels/ManageViewModels.cs
@@ -0,0 +1,35 @@
+﻿using ImpressDev.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ImpressDev.ViewModels
+{
+    public class ManageCredentialsViewModel
+    {
+        public ChangePasswordViewModel ChangePasswordViewModel { get; set; }
+        public ImpressDev.Controllers.ManageController.ManageMessageId? Message { get; set; }
+        public UserData UserData { get; set; }
+    }
+
+    public class ChangePasswordViewModel
+    {
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Aktualne hasło")]
+        public string OldPassword { get; set; }
+
+        [Required]
+        [StringLength(100, ErrorMessage = "Zbyt mała liczba znaków.", MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        [Display(Name = "Nowe hasło")]
+        public string NewPassword { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Potwierdź nowe hasło")]
+        [Compare("NewPassword", ErrorMessage = "Hasła nie pasują do siebie.")]
+        public string ConfirmPassword { get; set; }
+    }
+}
